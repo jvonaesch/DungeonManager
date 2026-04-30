747b15519950503e830d52e9a0bfa8fba84f2724
FenrisFox <jonas.vonaesch@gmail.com>
2026-04-30 11:25:22 +0200

    finished up prep for restructuring

--- src/dungeonmanager/session/SessionJsonSerializer.java
+++ src/dungeonmanager/session/SessionJsonSerializer.java
@@ -17,6 +17,8 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
+@Deprecated
+// TODO: get rid of this. Sessions will re-hydrate over their workspace directory
 public class SessionJsonSerializer {
 
     private static final Logger LOG = LoggerFactory.getLogger(SessionJsonSerializer.class);
